a1fd2acfc247066b08b3865134c305ff7673a427
Shayoon <quentin.degorgue@gmail.com>
2022-06-28 11:33:55 +0200

    fin factory pianiste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,21 @@ package org.example;
 import org.example.entity.Guitare;
 import org.example.entity.Guitariste;
 import org.example.entity.Musicien;
+import org.example.factory.MusicienFactory;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("================ dependance directe =============");
         dependanceDirecte();
+        System.out.println("================ Injection de dependance =============");
+        injectionDependance();
+    }
+
+    public static void injectionDependance(){
+        Musicien leMusicien = MusicienFactory.getGuitariste("Michel", "MicheloGuitare");
+        leMusicien.jouer();
+        Musicien lePianiste = MusicienFactory.getPianiste("Fred","Yamaha");
+        lePianiste.jouer();
     }
 
     private static void dependanceDirecte() {
